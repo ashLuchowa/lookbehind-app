ed33eb78d11fa50af11153f1f348005840998087
ashLuchowa <ashwinluchowa@gmail.com>
2026-01-20 20:55:51 +0800

    Add array to TextInfo

--- app/components/textInfo.tsx
+++ app/components/textInfo.tsx
@@ -1,20 +1,40 @@
 'use client'
+import { useState } from "react";
 
 type textInfo = {
     title: string
     textPara: string
 }
 
-export default function TextInfo({ title, textPara }: textInfo) {
-    const addInfo = (title, textPara):textInfo => {
-        // Placeholder function to demonstrate functionality
-    }
+export default function TextInfo() {
+    const [infos, setInfos] = useState<textInfo[]>([
+        {
+            title: 'Goal',
+            textPara: 'I would like to improve my yearly income to $100,000 before 2027',
+        },{
+            title: 'Tomorrow',
+            textPara: 'Buy carrots for piko',
+        },
+    ]);
+
+    // const addInfo = (title: string, textPara: string) => {
+    //     // Add form and button from a separate component
+    //     setInfos(prev => [
+    //         ...prev,
+    //         { title, textPara }
+    //     ]);
+    // };
 
     return (
-        <div className="text-info grid grid-cols-1 gap-4 bg-white shadow-sm p-4 rounded-3xl">
-            <h2 className="font-bold">{title}</h2>
-            <p>{textPara}</p>
-            <button onClick={() => addInfo(title, textPara)}>Click</button>
+        <div className="text-info grid grid-cols-1 gap-4">
+            {infos.map((info, index)=> (
+                <div key={index}>
+                    <div className="flex flex-col gap-1 bg-white shadow-sm p-4 rounded-3xl">
+                        <h2 className="font-bold">{info.title}</h2>
+                        <p>{info.textPara}</p>
+                    </div>
+                </div>
+            ))}
         </div>
     )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-center gap-6 p-6 bg-gray-100 w-150 min-h-screen mx-auto">
       <MainHeader nickname="Ash" age={36} weight={95} />
-      <TextInfo title="Goal" textPara="I would like to improve my yearly income to $100,000 before 2027" />
+      <TextInfo/>
     </div>
   );
 }
