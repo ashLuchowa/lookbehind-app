f8d32c82cf94608b7390e2ef6456c0021ac9391b
ashLuchowa <ashwinluchowa@gmail.com>
2026-01-20 22:11:16 +0800

    Add idCounter++

--- app/components/textInfo.tsx
+++ app/components/textInfo.tsx
@@ -2,16 +2,21 @@
 import { useState } from "react";
 
 type textInfo = {
+    id: number
     title: string
     textPara: string
 }
 
+let idCounter = 1;
+
 export default function TextInfo() {
     const [infos, setInfos] = useState<textInfo[]>([
         {
+            id: idCounter++,
             title: 'Goal',
             textPara: 'I would like to improve my yearly income to $100,000 before 2027',
         },{
+            id: idCounter++,
             title: 'Tomorrow',
             textPara: 'Buy carrots for piko',
         },
@@ -27,8 +32,8 @@ export default function TextInfo() {
 
     return (
         <div className="text-info grid grid-cols-1 gap-4">
-            {infos.map((info, index)=> (
-                <div key={index}>
+            {infos.map((info)=> (
+                <div key={info.id}>
                     <div className="flex flex-col gap-1 bg-white shadow-sm p-4 rounded-3xl">
                         <h2 className="font-bold">{info.title}</h2>
                         <p>{info.textPara}</p>
