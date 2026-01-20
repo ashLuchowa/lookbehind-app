5e7b967d47b8349c7781c62a8eb20ceea1ffe4ac
ashLuchowa <ashwinluchowa@gmail.com>
2026-01-21 00:46:09 +0800

    Get value from form

--- app/components/addform.tsx
+++ app/components/addform.tsx
@@ -1,13 +1,36 @@
+'use client';
+
+import { useState } from "react";
+
 export default function AddForm() {
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        // Replace console.log with actual submission logic
+        console.log(`Title: ${title}`);
+        console.log(`Description: ${description}`);
+    }
 
     return (
-        <form className="add-form flex flex-col gap-1 bg-white shadow-sm p-4 rounded-3xl">
+        <form 
+            onSubmit={handleSubmit} 
+            className="add-form flex flex-col gap-1 bg-white shadow-sm p-4 rounded-3xl">
             <input 
                 type="text" 
-                placeholder="Add title"
+                placeholder="title"
                 className="border"
+                onChange={(e) => setTitle(e.target.value)}
             />
-            <button type="submit">Submit</button>
+            <input 
+                type="text" 
+                placeholder="Description"
+                className="border"
+                onChange={(e) => setDescription(e.target.value)}
+            />
+            <button type="submit" className="bg-blue-500 cursor-pointer">Submit</button>
         </form>
     )
 }
